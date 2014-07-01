fefcb31faa71423717697199984866079f6c4051
Mathias Carignani <mathcarignani@gmail.com>
2014-07-01 12:11:50 -0300

    Animation posts

--- CiudadInvisible/controllers/PostsViewController.swift
+++ CiudadInvisible/controllers/PostsViewController.swift
@@ -12,6 +12,8 @@ class PostsViewController: UIViewController {
     
     // MARK: Outlets
     var centerPostView : PostView = PostView()
+    var leftPostView : PostView = PostView()
+    var rightPostView : PostView = PostView()
     
     @IBOutlet var cameraButton : UIButton
     
@@ -28,18 +30,33 @@ class PostsViewController: UIViewController {
         
         centerPostView = PostView(frame: view.frame, image: UIImage(named: "bg1.jpg"), name: "El cierre", distance: "500 m")
         self.view.insertSubview(centerPostView, belowSubview: cameraButton)
+        
+        leftPostView = PostView(frame: view.frame, image: UIImage(named: "bg1.jpg"), name: "El cierre", distance: "500 m")
+        leftPostView.center = CGPoint(x: leftPostView.center.x - 320, y: leftPostView.center.y)
+        self.view.insertSubview(leftPostView, belowSubview: cameraButton)
+        
+        rightPostView = PostView(frame: view.frame, image: UIImage(named: "bg1.jpg"), name: "El cierre", distance: "500 m")
+        rightPostView.center = CGPoint(x: rightPostView.center.x + 320, y: rightPostView.center.y)
+        self.view.insertSubview(rightPostView, belowSubview: cameraButton)
        
     }
     
     func setupGestures() {
         
         let panGesture = UIPanGestureRecognizer(target: self, action: Selector("panHandled:"))
-        
         view.addGestureRecognizer(panGesture)
+        
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: Selector("tapHandled:"))
+        doubleTapGesture.numberOfTapsRequired = 2
+        view.addGestureRecognizer(doubleTapGesture)
+        
         
     }
     
     // MARK: Touch methods
+    func tapHandled (gesture : UITapGestureRecognizer) {
+        println("tapHandled")
+    }
     
     func panHandled (gesture : UIPanGestureRecognizer) {
         
@@ -49,6 +66,8 @@ class PostsViewController: UIViewController {
             UIView.animateWithDuration(0.2, animations: {
                 self.cameraButton.transform = CGAffineTransformMakeTranslation(0, 100)
                 self.centerPostView.transform = CGAffineTransformMakeScale(0.8, 0.8)
+                self.leftPostView.transform = CGAffineTransformMakeScale(0.8, 0.8)
+                self.rightPostView.transform = CGAffineTransformMakeScale(0.8, 0.8)
             })
             break;
         case UIGestureRecognizerState.Changed:
@@ -59,13 +78,17 @@ class PostsViewController: UIViewController {
             var scale = CGAffineTransformMakeScale(0.8, 0.8)
             var transform = CGAffineTransformConcat(translation, scale)
             centerPostView.transform = transform
+            leftPostView.transform = transform
+            rightPostView.transform = transform
             
             break;
         case UIGestureRecognizerState.Ended:
         
             UIView.animateWithDuration(0.2, animations: {
                 self.cameraButton.transform = CGAffineTransformMakeTranslation(0, 0)
-                self.centerPostView.transform = CGAffineTransformMakeScale(1, 1)
+                self.centerPostView.transform = CGAffineTransformIdentity
+                self.leftPostView.transform = CGAffineTransformIdentity
+                self.rightPostView.transform = CGAffineTransformIdentity
                 })
             break;
         default:
